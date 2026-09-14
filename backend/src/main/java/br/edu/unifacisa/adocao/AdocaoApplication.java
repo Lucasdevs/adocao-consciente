56d485539036a147ee483373ee46c54855e5e458
Lucas Costa Teotônio <69512117+Lucasdevs@users.noreply.github.com>
2026-09-14 15:12:19 -0300

    update v2

--- backend/src/main/java/br/edu/unifacisa/adocao/AdocaoApplication.java
+++ backend/src/main/java/br/edu/unifacisa/adocao/AdocaoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(exclude=org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class)
 public class AdocaoApplication { 
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         SpringApplication.run(AdocaoApplication.class, args); 
     }
 }
